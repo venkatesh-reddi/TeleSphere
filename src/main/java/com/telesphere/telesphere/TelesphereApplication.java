368ec28527e4350e0860a4442a46f335a2a8390c
venkatesh-reddi <venkateshreddi@outlook.com>
2024-08-15 12:26:24 +0530

    Created Spring Boot Application.

---
+++ src/main/java/com/telesphere/telesphere/TelesphereApplication.java
@@ -0,0 +1,13 @@
+package com.telesphere.telesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelesphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelesphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telesphere/telesphere/TelesphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telesphere.telesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelesphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
